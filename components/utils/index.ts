b98c49850c515e4a2f861f29250bb0cb945ba8d6
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-12-23 23:55:38 -0500

    Add dashboard menu

---
+++ components/utils/index.ts
@@ -0,0 +1,33 @@
+
+const menus = [
+        {
+            title: "Dashboard",
+            icon: <MdDashboard />,
+            href: "/admin/dashboard",
+        },
+        {
+            title: "Products",
+            icon: <RiShoppingCartLine />,
+            href: "/admin/products",
+        },
+        {
+            title: "Accounts",
+            icon: <MdManageAccounts />,
+            href: "#",
+        },
+        {
+            title: "Transactions",
+            icon: <GrTransaction />,
+            href: "#",
+        },
+        {
+            title: "Analytics",
+            icon: <IoAnalytics/>,
+            href: "#"
+        },
+        {
+            title: "Setting",
+            icon: <IoSettings />,
+            href: "#",
+        },
+    ];
